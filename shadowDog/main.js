08cd756546dc79fca85c1fe023f8864174bc8e5d
Alexander_Ovchar <azalexxx02@gmail.com>
2024-02-04 21:17:36 +0200

    shadow animations complete

--- shadowDog/main.js
+++ shadowDog/main.js
@@ -1,3 +1,10 @@
+let playState = "idle";
+const dropdown = document.getElementById("animations");
+dropdown.addEventListener("change", function (e) {
+  playState = e.target.value;
+  console.log(playState);
+});
+
 const canvas = document.getElementById("canvas1");
 const ctx = canvas.getContext("2d");
 
@@ -6,31 +13,88 @@ const CANVAS_HEIGHT = (canvas.height = 600);
 
 const playerImage = new Image();
 playerImage.src = "shadow_dog.png";
-const stripeWidth = 575;
+const spriteWidth = 575;
 const stripeHeight = 523;
-let frameX = 0;
-let frameY = 0;
+
 let gameFrame = 0;
 const staggerFrames = 5;
+const spriteAnimations = [];
+const animationStates = [
+  {
+    name: "idle",
+    frames: 7,
+  },
+  {
+    name: "jump",
+    frames: 7,
+  },
+  {
+    name: "fall",
+    frames: 7,
+  },
+  {
+    name: "run",
+    frames: 9,
+  },
+  {
+    name: "dizzy",
+    frames: 11,
+  },
+  {
+    name: "sit",
+    frames: 5,
+  },
+  {
+    name: "roll",
+    frames: 7,
+  },
+  {
+    name: "bite",
+    frames: 7,
+  },
+  {
+    name: "ko",
+    frames: 7,
+  },
+  {
+    name: "getHit",
+    frames: 7,
+  },
+];
+
+animationStates.forEach((state, index) => {
+  let frames = {
+    loc: [],
+  };
+  for (let j = 0; j < state.frames; j++) {
+    let positionX = j * spriteWidth;
+    let positionY = index * stripeHeight;
+    frames.loc.push({ x: positionX, y: positionY });
+  }
+  spriteAnimations[state.name] = frames;
+});
+console.log(spriteAnimations);
 
 function animate() {
   ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  let position =
+    Math.floor(gameFrame / staggerFrames) %
+    spriteAnimations[playState].loc.length;
+
+  let frameX = spriteWidth * position;
+  let frameY = spriteAnimations[playState].loc[position].y;
+  frameX = position * spriteWidth;
   ctx.drawImage(
     playerImage,
-    frameX * stripeWidth,
-    frameY * stripeHeight,
-    stripeWidth,
+    frameX,
+    frameY,
+    spriteWidth,
     stripeHeight,
     0,
     0,
     CANVAS_WIDTH,
     CANVAS_HEIGHT
   );
-
-  if (gameFrame % staggerFrames === 0) {
-    if (frameX < 6) frameX++;
-    else frameX = 0;
-  }
 
   gameFrame++;
   requestAnimationFrame(animate);
