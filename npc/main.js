f1241d211b090bb72af3cda6bfe1596a2ff78a47
Alexander_Ovchar <azalexxx02@gmail.com>
2024-02-05 02:22:42 +0200

    add npc

---
+++ npc/main.js
@@ -0,0 +1,56 @@
+const canvas = document.getElementById("canvas1");
+const ctx = canvas.getContext("2d");
+
+const CANVAS_WIDTH = (canvas.width = 500);
+const CANVAS_HEIGHT = (canvas.height = 1000);
+
+const numberOfEnemies = 100;
+const enemiesArray = [];
+
+const enemyImage = new Image();
+enemyImage.src = "./enemies/enemy1.png";
+
+class Enemy {
+  constructor() {
+    this.x = Math.random() * CANVAS_WIDTH;
+    this.y = Math.random() * CANVAS_HEIGHT;
+    this.width = 100;
+    this.height = 100;
+    this.speed = Math.random() * 4 - 2;
+    this.spriteWidth = 293;
+    this.spriteHeight = 155;
+  }
+  update() {
+    this.x += this.speed;
+    this.y += this.speed;
+  }
+  draw() {
+    ctx.strokeRect(this.x, this.y, this.width, this.height);
+    ctx.drawImage(
+      enemyImage,
+      0,
+      0,
+      this.spriteWidth,
+      this.spriteHeight,
+      this.x,
+      this.y,
+      this.width,
+      this.height
+    );
+  }
+}
+
+for (let i = 0; i < numberOfEnemies; i++) {
+  enemiesArray.push(new Enemy());
+}
+
+function animate() {
+  ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  enemiesArray.forEach((enemy) => {
+    enemy.update();
+    enemy.draw();
+  });
+  requestAnimationFrame(animate);
+}
+
+animate();
