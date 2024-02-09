3f6e617bcea53646497ec978613b827afd3ba4cd
Alexander_Ovchar <azalexxx02@gmail.com>
2024-02-09 19:43:19 +0200

    add 3 type enemy

--- npc/main.js
+++ npc/main.js
@@ -4,7 +4,7 @@ const ctx = canvas.getContext("2d");
 const CANVAS_WIDTH = (canvas.width = 500);
 const CANVAS_HEIGHT = (canvas.height = 1000);
 
-const numberOfEnemies = 10;
+const numberOfEnemies = 100;
 const enemiesArray = [];
 
 let gameFrame = 0;
@@ -12,9 +12,9 @@ let gameFrame = 0;
 class Enemy {
   constructor() {
     this.image = new Image();
-    this.image.src = "./enemies/enemy2.png";
+    this.image.src = "./enemies/enemy3.png";
     this.speed = Math.random() * 4 + 1;
-    this.spriteWidth = 266;
+    this.spriteWidth = 218;
     this.spriteHeight = 188;
     this.width = this.spriteWidth / 2;
     this.height = this.spriteHeight / 2;
@@ -22,13 +22,17 @@ class Enemy {
     this.y = Math.random() * (CANVAS_HEIGHT - this.height);
     this.frame = 0;
     this.flapSpeed = Math.floor(Math.random() * 3 + 1);
-    this.angle = Math.random() * 2;
-    this.angelSpeed = Math.random() * 0.2;
-    this.curve = Math.random() * 7;
+    this.angle = Math.random() * 500;
+    this.angelSpeed = Math.random() * 0.5 + 0.5;
+    // this.curve = Math.random() * 200 + 50;
   }
   update() {
-    this.x -= this.speed;
-    this.y += this.curve * Math.sin(this.angle);
+    this.x =
+      (CANVAS_WIDTH / 2) * Math.cos((this.angle * Math.PI) / 200) +
+      (CANVAS_WIDTH / 2 - this.width / 2);
+    this.y =
+      (CANVAS_HEIGHT / 2) * Math.sin((this.angle * Math.PI) / 300) +
+      (CANVAS_HEIGHT / 2 - this.height / 2);
     this.angle += this.angelSpeed;
     if (this.x + this.width < 0) {
       this.x = CANVAS_WIDTH;
